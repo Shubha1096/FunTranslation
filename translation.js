d4d7db2f03df712e53b0d4c112128bd2327b551c
Shubha1096 <74348224+Shubha1096@users.noreply.github.com>
2020-12-12 14:08:46 +0900

    final :Commit

--- translation.js
+++ translation.js
@@ -1,24 +1,23 @@
 var btnTranslate = document.querySelector("#translate-btn");
-
-btnTranslate.addEventListener("click", buttonClickHandler)
-
 var txtInput = document.querySelector("#input");
 var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function ConversionURL(input) {
+
+function TranslationURL(input) {
     return serverURL + "?" + "text=" + input
 }
 
-function buttonClickHandler() {
-    var inputText = txtInput.value; 
+function clickHandler() {
+    var inputText = txtInput.value;
 
-  
-    fetch(ConversionURL(inputText))
+    fetch(TranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
             outputDiv.innerText = translatedText;
            })
+
 };
+btnTranslate.addEventListener("click", clickHandler)
